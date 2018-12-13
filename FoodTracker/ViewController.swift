0394012a5e08be99a06f3a017e923cedd57f4aa5
cnaro1996 <cnaro1996@gmail.com>
2018-12-12 19:13:41 -0500

    Update finish "Implement a Custom Control" section

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -42,12 +42,15 @@ UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         // The info dictionary may contain multiple representations of the image. You want to use the original.
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
-            else {
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
                 fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
         
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
         
+        // Dimiss the picker.
+        dismiss(animated: true, completion: nil)
     }
     
     //MARK: Actions
