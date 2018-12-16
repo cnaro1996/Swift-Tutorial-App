e43d5b7d545ea119b975264a419591656cfd35c4
cnaro1996 <cnaro1996@gmail.com>
2018-12-15 23:53:45 -0500

    Update finish "Implement a Custom Control" section

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ UINavigationControllerDelegate {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,10 +73,5 @@ UINavigationControllerDelegate {
         present(imagePickerController, animated: true, completion: nil)
         
     }
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 }
 
